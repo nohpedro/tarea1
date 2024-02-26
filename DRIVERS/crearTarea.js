e6429445e4a85ffdb1613726f6cae2447f4cdccc
nohpedro <147351155+nohpedro@users.noreply.github.com>
2024-02-26 11:07:34 -0400

    a
    a

--- DRIVERS/crearTarea.js
+++ DRIVERS/crearTarea.js
@@ -1,19 +1,18 @@
 function crearNuevaTarea() {
-    const nombre = document.getElementById("nombreTarea").value;
-    const horaInicio = document.getElementById("horaInicio").value;
-    const horaFin = document.getElementById("horaFin").value;
-  
-    // Generar un nuevo código para la tarea (puedes hacerlo de manera automática)
-    const nuevoCodigo = tareas.length + 1;
-    
-  
-    // Añadir la nueva tarea al array
-    tareas.push([nuevoCodigo, nombre, horaInicio, horaFin, "Por Iniciar"]);
+  const nombre = document.getElementById("nombreTarea").value;
+  const horaInicio = document.getElementById("horaInicio").value;
+  const horaFin = document.getElementById("horaFin").value;
 
-    document.getElementById("nombreTarea").value = "";
-    document.getElementById("horaInicio").value = "";
-    document.getElementById("horaFin").value = "";
-  
-    // Mostrar las tareas actualizadas
-    mostrarTareas();
-  }
+  // Generar un nuevo código para la tarea (puedes hacerlo de manera automática)
+  const nuevoCodigo = tareas.length + 1;
+
+  // Añadir la nueva tarea al array
+  tareas.push([nuevoCodigo, nombre, horaInicio, horaFin, "Nueva"]);
+
+  document.getElementById("nombreTarea").value = "";
+  document.getElementById("horaInicio").value = "";
+  document.getElementById("horaFin").value = "";
+
+  // Mostrar las tareas actualizadas
+  mostrarTareas();
+}
