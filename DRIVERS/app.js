3d82ce12244a56808b49f2bc4c53e63bed52d8a3
nohpedro <147351155+nohpedro@users.noreply.github.com>
2024-03-15 10:51:37 -0400

    V 2.2
    Generar reporte de tareas eliminadas.
    Buscar bugs

--- DRIVERS/app.js
+++ DRIVERS/app.js
@@ -4,6 +4,129 @@ let tareas = [
   [3, "Hacer deberes", "12:00", "14:00", "Nueva"],
   [4, "Ejercicio", "12:20", "14:00", "Completada"],
 ];
+let eliminadas = [
+  ["001", "Tarea 1", "10:00", "11:30", "Completada"],
+  ["002", "Tarea 2", "12:00", "13:30", "Plazo Vencido"],
+  ["003", "Tarea 3", "14:00", "15:30", "Plazo Vencido"],
+];
+
+let tareaSeleccionada = null;
+
+function generarHistorial() {
+  let tablaBody = document.getElementById("tablaBody");
+  tablaBody.innerHTML = ""; // Limpiar contenido previo de la tabla
+
+  eliminadas.forEach((tarea) => {
+    let fila = document.createElement("tr");
+
+    tarea.forEach((dato, i) => {
+      if (i != 0) {
+        let celda = document.createElement("td");
+        celda.textContent = dato;
+        fila.appendChild(celda);
+      }
+    });
+
+    fila.addEventListener("click", (e) => {
+      let selectedRows = tablaBody.querySelectorAll("tr.selected");
+      selectedRows.forEach((row) => {
+        row.classList.remove("selected");
+      });
+
+      fila.classList.add("selected");
+    });
+
+    fila.addEventListener("mouseover", (e) => {
+      if (!fila.classList.contains("selected")) {
+        fila.classList.add("hover");
+      }
+    });
+
+    fila.addEventListener("mouseout", (e) => {
+      if (!fila.classList.contains("selected")) {
+        fila.classList.remove("hover");
+      }
+    });
+
+    fila.setAttribute("cod", tarea[0]);
+
+    fila.addEventListener("click", (e) => {
+      let nombre = fila.cells[0].textContent;
+      let horaInicio = fila.cells[1].textContent;
+      let horaFinal = fila.cells[2].textContent;
+
+      tareaSeleccionada = [nombre, horaInicio, horaFinal];
+
+      console.log(tareaSeleccionada);
+    });
+
+    tablaBody.appendChild(fila);
+  });
+}
+
+function reestablecerTarea() {
+  if (tareaSeleccionada == null) {
+    alert("Seleccione una tarea");
+    return;
+  }
+
+  cambiarSeccion("editarTarea");
+  document.getElementById("nombreTarea").value = tareaSeleccionada[0];
+  document.getElementById("horaInicio").value = tareaSeleccionada[1];
+  document.getElementById("horaFin").value = tareaSeleccionada[2];
+
+  console.log(tareaSeleccionada);
+  //eliminadas.slice();
+  document.getElementById("editNombre").value = "editada";
+  tareaSeleccionada = null;
+}
+
+function saveTasks() {
+  localStorage.setItem(
+    "tasks",
+    JSON.stringify({
+      tpendientes: tareas,
+      teliminadas: eliminadas,
+    })
+  );
+}
+
+function loadTasks() {
+  const storedTasks = localStorage.getItem("tasks");
+  if (storedTasks) {
+    tareas = JSON.parse(storedTasks.tpendientes);
+    eliminadas = JSON.parse(storedTasks.teliminadas);
+  }
+}
+
+function periodicCheck() {
+  let currentTime = new Date();
+  let currentHour = currentTime.getHours();
+  let currentMinute = currentTime.getMinutes();
+
+  let totalSeconds = currentHour * 3600 + currentMinute * 60;
+
+  tareas.forEach((tarea, index) => {
+    let [hour, minute] = tarea[3].split(":").map(Number);
+
+    let horaFinhour = hour;
+    let horaFinmin = minute;
+
+    let horaFin = horaFinhour * 3600 + horaFinmin * 60;
+
+    if (totalSeconds > horaFin) {
+      alert(`Tarea "${tarea[1]}" ha expirado.`);
+      tareas.splice(index, 1);
+    }
+  });
+
+  console.log("check");
+}
+
+loadTasks();
+periodicCheck();
+
+let intervalId = setInterval(periodicCheck, 60 * 1000);
 
 function cargarTareaParaEditar() {
   const selectCodigo = document.getElementById("codigoTarea");
