ee065c53f68361802654a1c554553855a63b933f
SebasMagno1 <114171307+SebasMagno1@users.noreply.github.com>
2024-03-14 22:53:00 -0400

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,16 @@
+function cambiarSeccion(seccionId) {
+    document.querySelectorAll('.pagina').forEach(function(page) {
+      page.style.display = 'none';
+    });
+    document.getElementById(seccionId).style.display = 'block';
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('navCrearTarea').addEventListener('click', function() { cambiarSeccion('crearTarea'); });
+    document.getElementById('navEditarTarea').addEventListener('click', function() { cambiarSeccion('editarTarea'); });
+    document.getElementById('navEliminarTarea').addEventListener('click', function() { cambiarSeccion('eliminarTarea'); });
+    document.getElementById('navAgregarAtributos').addEventListener('click', function() { cambiarSeccion('agregarAtributos'); });
+    document.getElementById('navGenerarReporte').addEventListener('click', function() { cambiarSeccion('generarReporte'); });
+
+    cambiarSeccion('crearTarea');
+  });
